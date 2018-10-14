f732653f35f8599c2a00a16024ba79cf336c411d
Alexei Sersun <alex.sersun@yahoo.com>
2018-10-14 14:27:32 +0300

    Add handlers for incoming and outcoming messages.

--- client/client.go
+++ client/client.go
@@ -9,40 +9,77 @@ import (
 
 // Client ...
 type Client struct {
-	Protocol   string
-	Address    string
-	Connection net.Conn
+	Protocol    string
+	Address     string
+	connection  net.Conn
+	incomingMsg chan string
+	outcominMsg chan string
 }
 
 // Init ...
 func (c *Client) Init(protocol string, address string) {
 	c.Protocol = protocol
 	c.Address = address
-	var err error
-	c.Connection, err = net.Dial(c.Protocol, c.Address)
+	conn, err := net.Dial(c.Protocol, c.Address)
+	c.connection = conn
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	c.incomingMsg = make(chan string)
+	c.outcominMsg = make(chan string)
 }
 
 // Run ...
 func (c *Client) Run() {
+	go c.listenIncomingMessages()
+	go c.listenOutcomingMessages()
+
 	for {
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(">>")
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
+		select {
+		case newIncomingMessage := <-c.incomingMsg:
+			c.handleIncomingMessage(newIncomingMessage)
+		case newOutcomindMessage := <-c.outcominMsg:
+			c.handleOutcomingMessage(newOutcomindMessage)
 		}
-		// send to socket
-		fmt.Fprintf(c.Connection, text+"\n")
-		// listen for reply
-		message, err := bufio.NewReader(c.Connection).ReadString('\n')
+	}
+
+}
+
+func (c *Client) handleIncomingMessage(message string) {
+	fmt.Println("incoming msg: " + message)
+}
+
+func (c *Client) handleOutcomingMessage(message string) {
+	fmt.Println("outcoming msg: " + message)
+	c.connection.Write([]byte(message))
+}
+
+func (c *Client) send(data string) {
+
+}
+
+func (c *Client) get() string {
+	return ""
+}
+
+func (c *Client) listenIncomingMessages() {
+	reader := bufio.NewReader(c.connection)
+	c.listen(reader, c.incomingMsg)
+}
+
+func (c *Client) listenOutcomingMessages() {
+	reader := bufio.NewReader(os.Stdin)
+	c.listen(reader, c.outcominMsg)
+}
+
+func (c *Client) listen(reader *bufio.Reader, channel chan string) {
+	for {
+		message, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println(err, message)
 		}
-		fmt.Println(message)
+		channel <- message
 	}
 }
