89ce31272e4d51647ab372d737173fb027466cdb
Alexei Sersun <alex.sersun@yahoo.com>
2018-10-15 22:55:53 +0300

    Implement publish-subscribe mechanism.

--- broker/publish-command.go
+++ broker/publish-command.go
@@ -2,6 +2,7 @@ package broker
 
 import (
 	"PAD-151-Message-Broker/model"
+	"log"
 )
 
 // PublishCommand - send broadcast messages
@@ -12,5 +13,20 @@ type PublishCommand struct {
 
 // Execute - send
 func (pc PublishCommand) Execute() {
+	publisherID := pc.message.SenderID
+	log.Printf("Clien %v posted", publisherID)
+	responseMessageModel := model.ResponseMessageModel{
+		pc.message.SenderID,
+		pc.message.Type,
+		-1,
+		pc.message.Message,
+	}
+
+	for _, v := range pc.broker.subscribers[publisherID] {
+		user := pc.broker.userMap[v]
+		go sendMessage(user, responseMessageModel, pc.broker.deadUserIds)
+	}
+
+	// Send message to specified user
 
 }
